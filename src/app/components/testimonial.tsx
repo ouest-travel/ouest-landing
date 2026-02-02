bb96441a5188cf3f1d02e8cd0f69b42a8dec4f2b
deiay <davidaideyan@gmail.com>
2026-02-01 23:09:33 -0800

    update image

--- src/app/components/testimonial.tsx
+++ src/app/components/testimonial.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import founderImage from "figma:asset/272d85223bba423f76848abe3fb77c0b51985942.png";
+import founderImage from "figma:asset/8a2af99c66bf19bf43994adbffe8d7041d3aaeee.png";
 import { GradientText } from "./ui-components";
 
 export function Testimonial() {
@@ -7,13 +7,13 @@ export function Testimonial() {
     <section className="max-w-[1500px] mx-auto px-[40px] pb-[120px]">
       <div className="flex flex-col lg:flex-row gap-[20px]">
         <div className="flex-1 aspect-[550/624] relative rounded-[30px] overflow-hidden group">
-          <img 
-            src={founderImage} 
-            alt="Timmy Afolabi - Founder & CEO of Ouest" 
+          <img
+            src={founderImage}
+            alt="Timmy Afolabi - Founder & CEO of Ouest"
             className="size-full object-cover transition-transform duration-700 group-hover:scale-105"
           />
         </div>
-        
+
         <div className="flex-1 border-t border-[#e9e9e9] pt-[60px] flex flex-col justify-center pl-0 lg:pl-[50px]">
           <blockquote className="font-['Bernino_Sans:Semibold',sans-serif] text-[40px] leading-tight tracking-[-1.6px] text-black mb-[50px]">
             “Born out of redefining travel planning, my focus is on building a smart and seamless experience with personalization and community at the center.”
